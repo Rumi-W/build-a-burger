d2add2a519a87961e8f276fb33a4ac51b265c65e
Harumi Warner <harumi@harumis-mbp.home>
2019-11-19 17:27:47 -0500

    Updates

---
+++ precache-manifest.a03f0ae56f680271d5027fdb5ba170c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee435c738a59a57a2a3e40d3b765d121",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "edbaa3cfd1d2141676ff",
+    "url": "/my-app/static/css/main.5bdc0cce.chunk.css"
+  },
+  {
+    "revision": "d5ce4db4724128c0e2ad",
+    "url": "/my-app/static/js/2.3798b8fe.chunk.js"
+  },
+  {
+    "revision": "edbaa3cfd1d2141676ff",
+    "url": "/my-app/static/js/main.f40ef5be.chunk.js"
+  },
+  {
+    "revision": "c4b62e82e2c572c723b3",
+    "url": "/my-app/static/js/runtime-main.2b8798af.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/my-app/static/media/burger-logo.b8503d26.png"
+  }
+]);
