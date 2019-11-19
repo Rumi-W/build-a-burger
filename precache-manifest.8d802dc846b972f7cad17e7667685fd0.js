c5051f1be473f5c1f90cd5542cb08af550d28289
Harumi Warner <harumi@harumis-mbp.home>
2019-11-19 17:37:52 -0500

    Updates

---
+++ precache-manifest.8d802dc846b972f7cad17e7667685fd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae317563aafb67468cf2c18ce88594fa",
+    "url": "/build-a-burger/index.html"
+  },
+  {
+    "revision": "260a45bd214ff060cb52",
+    "url": "/build-a-burger/static/css/main.5bdc0cce.chunk.css"
+  },
+  {
+    "revision": "d5ce4db4724128c0e2ad",
+    "url": "/build-a-burger/static/js/2.3798b8fe.chunk.js"
+  },
+  {
+    "revision": "260a45bd214ff060cb52",
+    "url": "/build-a-burger/static/js/main.30b3755e.chunk.js"
+  },
+  {
+    "revision": "529eff8c6b1305df4b28",
+    "url": "/build-a-burger/static/js/runtime-main.bae570ad.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/build-a-burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.a03f0ae56f680271d5027fdb5ba170c2.js"
+  "/build-a-burger/precache-manifest.8d802dc846b972f7cad17e7667685fd0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build-a-burger/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
